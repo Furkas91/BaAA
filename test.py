a16b7f3d700fbda1abc6942862e4a4d337ab614c
Furkas91 <andrey98vas@gmail.com>
2021-04-05 01:02:18 +0300

    Добавлена наивная реализация бинарного и декартова деревьев

--- test.py
+++ test.py
@@ -5,21 +5,16 @@ from typing import TextIO
 
 from algorithms.BSearch import binary_search
 from algorithms.BSearchAns import binary_search_by_answer
+from algorithms.BinaryTree import BinaryTree, Treap
 from algorithms.DisjointSetUnion import Node, unite, text_test
 from algorithms.addition import addition
 
 ADDING_TESTS = 'tests\\addition'
-
 BINARY_SEARCH_TESTS = 'tests\\binary-search'
-
 BINARY_SEARCH_BY_RESULTS_TESTS = 'tests\\binary-search-by-results'
-
-BINARY_TREE_IN = [f"tests\\binary-tree\\{i}.in" for i in range(1, 8)]
-BINARY_TREE_CONTAINS_OUT = [f"tests\\binary-tree\\{i}.contains.out" for i in range(1, 8)]
-BINARY_TREE_MIN_AFTER_OUT = [f"tests\\binary-tree\\{i}.min-after.out" for i in range(1, 8)]
-
-UFF_TEST = 'tests\\uff'
-BLOCKS_ON_THE_PICTURE_TEST = 'tests\\blocks'
+UFF_TESTS = 'tests\\uff'
+BLOCKS_ON_THE_PICTURE_TESTS = 'tests\\blocks'
+BINARY_TREE = 'tests\\binary-tree'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -39,6 +34,35 @@ def generator_test_files(directory: str, log: Logger):
             yield fin, fout, file
 
 
+def generator_test_files_for_binary_tree(directory: str, log: Logger):
+    """ Генератор входных и проверочных файлов для тестов деревьев.
+
+    :type log: Logger
+    :type directory: str
+    """
+    for file in os.listdir(directory):
+        if file.endswith('.in'):
+            file = file[0:-3]
+            log.info(f"Test {file}...")
+            fin: TextIO = open(directory + '\\' + file + '.in', "r")
+            fout: TextIO = open(directory + '\\' + file + '.contains.out', "r")
+            fout2: TextIO = open(directory + '\\' + file + '.min-after.out', "r")
+
+            yield fin, fout, fout2, file
+
+
+def log_result_test(get_result, right_answer, test_name: str, log: Logger) -> bool:
+    """
+    Логирует ошибки если они есть, а также возвращет результат прохождения теста.
+
+    :rtype bool
+    """
+    if not get_result == right_answer:
+        log.info(
+            f"Test {test_name} failed: expected {right_answer}, were got {get_result}")
+    return get_result == right_answer
+
+
 def adding_test():
     log: Logger = logging.getLogger("Adding")
     for fin, fout, file in generator_test_files(ADDING_TESTS, log):
@@ -48,6 +72,7 @@ def adding_test():
             x, y = fin.readline().replace('\n', '').split(' ')
             get_result: str = addition(x, y)
             right_answer: str = fout.readline().replace('\n', '')
+            log.debug(f"File {file}: the program received {get_result}, when {right_answer} was expected.")
             if get_result != right_answer:
                 log.info(
                     f"Test {file} failed on the position {i} ({x, y}): expected {right_answer},"
@@ -75,6 +100,7 @@ def binary_search_test():
             val = int(fin.readline())
             right_answer: int = int(fout.readline())
             get_result: int = binary_search(array, val, right=n - 1)
+            log.debug(f"File {file}: the program received {get_result}, when {right_answer} was expected.")
             if get_result != right_answer:
                 log.info(
                     f"Test {file} failed on the position {i} ({val}): expected {right_answer},"
@@ -99,7 +125,7 @@ def binary_search_by_answer_test():
             array.append(int(fin.readline()))
         get_result: int = binary_search_by_answer(array, N, k, right=array[-1])
         right_answer: int = int(fout.readline())
-
+        log.debug(f"File {file}: the program received {get_result}, when {right_answer} was expected.")
         if (get_result != right_answer):
             log.info(
                 f"Test {file} failed: expected {right_answer}, were got {get_result}")
@@ -112,7 +138,7 @@ def binary_search_by_answer_test():
 
 def CHM_test():
     log = logging.getLogger("CHM")
-    for fin, fout, file in generator_test_files(UFF_TEST, log):
+    for fin, fout, file in generator_test_files(UFF_TESTS, log):
         n: list = fin.readline().split()
         dict = {}
         for i in range(int(n[-1])):
@@ -124,6 +150,7 @@ def CHM_test():
             if get_result == 'NO':
                 unite(dict[array[0]], dict[array[1]])
             right_answer = fout.readline().replace('\n', '')
+            log.debug(f"File {file}: the program received {get_result}, when {right_answer} was expected.")
             if not get_result == right_answer:
                 log.info(
                     f"Test {file} failed: expected {right_answer}, were got {get_result}")
@@ -135,21 +162,21 @@ def CHM_test():
 
 def blocks_on_the_picture():
     log = logging.getLogger('Picture')
-    for fin, fout, file in generator_test_files(BLOCKS_ON_THE_PICTURE_TEST, log):
+    for fin, fout, file in generator_test_files(BLOCKS_ON_THE_PICTURE_TESTS, log):
         n = fin.readline().split()
         prev = fin.readline().replace('\n', '')
         array = {}
         left = False
         for j in range(len(prev)):
             if prev[j] == '1':
-                array[(0,j)] = Node((0,j))
+                array[(0, j)] = Node((0, j))
                 if left:
                     unite(array[(0, j)], array[(0, j - 1)])
                 left = True
             else:
                 left = False
         left = False
-        for i in range(1,int(n[0])):
+        for i in range(1, int(n[0])):
             current = fin.readline().replace('\n', '')
             for j in range(len(current)):
                 if current[j] == '1':
@@ -157,8 +184,8 @@ def blocks_on_the_picture():
                     if left:
                         unite(array[(i, j)], array[(i, j - 1)])
                     if prev[j] == '1':
-                        unite(array[(i-1, j)], array[(i, j)])
-                    left=True
+                        unite(array[(i - 1, j)], array[(i, j)])
+                    left = True
                 else:
                     left = False
             prev = current
@@ -168,6 +195,7 @@ def blocks_on_the_picture():
             hash_table.add(i.find())
         get_result = len(hash_table)
         right_answer = int(fout.readline().replace('\n', ''))
+        log.debug(f"File {file}: the program received {get_result}, when {right_answer} was expected.")
         if not get_result == right_answer:
             log.info(
                 f"Test {file} failed: expected {right_answer}, were got {get_result}")
@@ -177,32 +205,51 @@ def blocks_on_the_picture():
         fout.close()
 
 
-def default_binary_tree_test():
-    """ Не рабочий кусок кода не тройгайте пожалуйста, возможно его починят"""
-    # TODO Сделать тестирование бинарного дерева
-    log = logging.getLogger("BinaryTree")
-    for file in range(len(BINARY_TREE_IN)):
-        log.info(f"Test {file + 1}...")
-        fin = open(BINARY_TREE_IN[file], "r")
-        fout = open(BINARY_TREE_CONTAINS_OUT[file], "r")
-        fout2 = open(BINARY_TREE_MIN_AFTER_OUT[file], "r")
-        N = int(fin.readline())
-        k = int(fin.readline())
-        array = []
-        for i in range(N):
-            array.append(int(fin.readline()))
-        get_result: int = binary_search_by_answer(array, N, k, right=array[-1])
-        right_answer: int = int(fout.readline())
+def _binary_tree_test_utils(fout, fout2, get_result_contains, get_result_min_after, file, log: Logger) -> bool:
+    right_answer_contains: str = fout.readline().replace('\n', '')
+    right_answer_min_after: str = fout2.readline().replace('\n', '')
+    flag = log_result_test(get_result_contains, right_answer_contains,
+                           '{}.{}'.format(file, 'contains'), log)
+    flag = flag if log_result_test(get_result_min_after, right_answer_min_after,
+                                   '{}.{}'.format(file, 'min-after'), log) else False
+    return flag
 
-        if (get_result != right_answer):
-            log.info(
-                f"Test {file + 1} failed: expected {right_answer}, were got {get_result}")
+
+def binary_tree_test(Tree):
+    """ Тестирует деревья, наследующие интерфейс BinaryTree, принимает в себя тестируемый класс"""
+    log = logging.getLogger(Tree.__name__)
+    for fin, fout, fout2, file in generator_test_files_for_binary_tree(BINARY_TREE, log):
+        n = int(fin.readline())
+        current_value = int(fin.readline())
+        tree = Tree(current_value)
+        get_result_contains = '-'
+        get_result_min_after = '- -'
+        _binary_tree_test_utils(fout, fout2, get_result_contains, get_result_min_after, file, log)
+        flag = True
+        for i in range(n - 1):
+            current_value = int(fin.readline())
+            try:
+                get_result_contains: str = tree.text_find_node(current_value)
+                if get_result_contains == '-':
+                    tree.add_node(current_value)
+                get_result_min_after = '{} {}'.format(get_result_contains,
+                                                      tree.text_find_next_node(current_value))
+            except RecursionError:
+                flag = False
+                log.info(f"Test {file} failed: the tree has too much depth")
+                break
+            flag = flag if _binary_tree_test_utils(fout,
+                                                   fout2,
+                                                   get_result_contains,
+                                                   get_result_min_after,
+                                                   file,
+                                                   log) else False
         else:
-            log.info(f"Test {file + 1} passed!")
+            if flag:
+                log.info(f"Test {file} passed!")
 
         fin.close()
         fout.close()
-
 
 
 if __name__ == '__main__':
@@ -210,4 +257,6 @@ if __name__ == '__main__':
     # binary_search_test()
     # binary_search_by_answer_test()
     # CHM_test()
-    blocks_on_the_picture()
+    # blocks_on_the_picture()
+    # binary_tree_test(BinaryTree)
+    binary_tree_test(Treap)
